13e1054d25ac1fd88a60f940662303484529e887
kantimasuk <kantimasuk@kkumail.com>
2026-01-11 23:27:13 +0700

    nanon2

--- apps/lab04-photo-vue/vite.config.ts
+++ apps/lab04-photo-vue/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/mobileweb/lab04_ionic/dist/',   // 👈 เพิ่มแค่นี้
   plugins: [
     vue(),
     legacy()
